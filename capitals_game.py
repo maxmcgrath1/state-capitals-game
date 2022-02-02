d300ace1cc63ef7544cdc6194db2602a286a70d0
maxmcgrath1 <tuc06096@temple.edu>
2022-02-01 19:53:14 -0500

    Added option to play again

--- capitals_game.py
+++ capitals_game.py
@@ -20,139 +20,140 @@ states = [
 }, {
     "name": "Colorado",
     "capital": "Denver"
-}, {
-    "name": "Connecticut",
-    "capital": "Hartford"
-}, {
-    "name": "Delaware",
-    "capital": "Dover"
-}, {
-    "name": "Florida",
-    "capital": "Tallahassee"
-}, {
-    "name": "Georgia",
-    "capital": "Atlanta"
-}, {
-    "name": "Hawaii",
-    "capital": "Honolulu"
-}, {
-    "name": "Idaho",
-    "capital": "Boise"
-}, {
-    "name": "Illinois",
-    "capital": "Springfield"
-}, {
-    "name": "Indiana",
-    "capital": "Indianapolis"
-}, {
-    "name": "Iowa",
-    "capital": "Des Moines"
-}, {
-    "name": "Kansas",
-    "capital": "Topeka"
-}, {
-    "name": "Kentucky",
-    "capital": "Frankfort"
-}, {
-    "name": "Louisiana",
-    "capital": "Baton Rouge"
-}, {
-    "name": "Maine",
-    "capital": "Augusta"
-}, {
-    "name": "Maryland",
-    "capital": "Annapolis"
-}, {
-    "name": "Massachusetts",
-    "capital": "Boston"
-}, {
-    "name": "Michigan",
-    "capital": "Lansing"
-}, {
-    "name": "Minnesota",
-    "capital": "St. Paul"
-}, {
-    "name": "Mississippi",
-    "capital": "Jackson"
-}, {
-    "name": "Missouri",
-    "capital": "Jefferson City"
-}, {
-    "name": "Montana",
-    "capital": "Helena"
-}, {
-    "name": "Nebraska",
-    "capital": "Lincoln"
-}, {
-    "name": "Nevada",
-    "capital": "Carson City"
-}, {
-    "name": "New Hampshire",
-    "capital": "Concord"
-}, {
-    "name": "New Jersey",
-    "capital": "Trenton"
-}, {
-    "name": "New Mexico",
-    "capital": "Santa Fe"
-}, {
-    "name": "New York",
-    "capital": "Albany"
-}, {
-    "name": "North Carolina",
-    "capital": "Raleigh"
-}, {
-    "name": "North Dakota",
-    "capital": "Bismarck"
-}, {
-    "name": "Ohio",
-    "capital": "Columbus"
-}, {
-    "name": "Oklahoma",
-    "capital": "Oklahoma City"
-}, {
-    "name": "Oregon",
-    "capital": "Salem"
-}, {
-    "name": "Pennsylvania",
-    "capital": "Harrisburg"
-}, {
-    "name": "Rhode Island",
-    "capital": "Providence"
-}, {
-    "name": "South Carolina",
-    "capital": "Columbia"
-}, {
-    "name": "South Dakota",
-    "capital": "Pierre"
-}, {
-    "name": "Tennessee",
-    "capital": "Nashville"
-}, {
-    "name": "Texas",
-    "capital": "Austin"
-}, {
-    "name": "Utah",
-    "capital": "Salt Lake City"
-}, {
-    "name": "Vermont",
-    "capital": "Montpelier"
-}, {
-    "name": "Virginia",
-    "capital": "Richmond"
-}, {
-    "name": "Washington",
-    "capital": "Olympia"
-}, {
-    "name": "West Virginia",
-    "capital": "Charleston"
-}, {
-    "name": "Wisconsin",
-    "capital": "Madison"
-}, {
-    "name": "Wyoming",
-    "capital": "Cheyenne"
-}]
+}] 
+# {
+#     "name": "Connecticut",
+#     "capital": "Hartford"
+# }, {
+#     "name": "Delaware",
+#     "capital": "Dover"
+# }, {
+#     "name": "Florida",
+#     "capital": "Tallahassee"
+# }, {
+#     "name": "Georgia",
+#     "capital": "Atlanta"
+# }, {
+#     "name": "Hawaii",
+#     "capital": "Honolulu"
+# }, {
+#     "name": "Idaho",
+#     "capital": "Boise"
+# }, {
+#     "name": "Illinois",
+#     "capital": "Springfield"
+# }, {
+#     "name": "Indiana",
+#     "capital": "Indianapolis"
+# }, {
+#     "name": "Iowa",
+#     "capital": "Des Moines"
+# }, {
+#     "name": "Kansas",
+#     "capital": "Topeka"
+# }, {
+#     "name": "Kentucky",
+#     "capital": "Frankfort"
+# }, {
+#     "name": "Louisiana",
+#     "capital": "Baton Rouge"
+# }, {
+#     "name": "Maine",
+#     "capital": "Augusta"
+# }, {
+#     "name": "Maryland",
+#     "capital": "Annapolis"
+# }, {
+#     "name": "Massachusetts",
+#     "capital": "Boston"
+# }, {
+#     "name": "Michigan",
+#     "capital": "Lansing"
+# }, {
+#     "name": "Minnesota",
+#     "capital": "St. Paul"
+# }, {
+#     "name": "Mississippi",
+#     "capital": "Jackson"
+# }, {
+#     "name": "Missouri",
+#     "capital": "Jefferson City"
+# }, {
+#     "name": "Montana",
+#     "capital": "Helena"
+# }, {
+#     "name": "Nebraska",
+#     "capital": "Lincoln"
+# }, {
+#     "name": "Nevada",
+#     "capital": "Carson City"
+# }, {
+#     "name": "New Hampshire",
+#     "capital": "Concord"
+# }, {
+#     "name": "New Jersey",
+#     "capital": "Trenton"
+# }, {
+#     "name": "New Mexico",
+#     "capital": "Santa Fe"
+# }, {
+#     "name": "New York",
+#     "capital": "Albany"
+# }, {
+#     "name": "North Carolina",
+#     "capital": "Raleigh"
+# }, {
+#     "name": "North Dakota",
+#     "capital": "Bismarck"
+# }, {
+#     "name": "Ohio",
+#     "capital": "Columbus"
+# }, {
+#     "name": "Oklahoma",
+#     "capital": "Oklahoma City"
+# }, {
+#     "name": "Oregon",
+#     "capital": "Salem"
+# }, {
+#     "name": "Pennsylvania",
+#     "capital": "Harrisburg"
+# }, {
+#     "name": "Rhode Island",
+#     "capital": "Providence"
+# }, {
+#     "name": "South Carolina",
+#     "capital": "Columbia"
+# }, {
+#     "name": "South Dakota",
+#     "capital": "Pierre"
+# }, {
+#     "name": "Tennessee",
+#     "capital": "Nashville"
+# }, {
+#     "name": "Texas",
+#     "capital": "Austin"
+# }, {
+#     "name": "Utah",
+#     "capital": "Salt Lake City"
+# }, {
+#     "name": "Vermont",
+#     "capital": "Montpelier"
+# }, {
+#     "name": "Virginia",
+#     "capital": "Richmond"
+# }, {
+#     "name": "Washington",
+#     "capital": "Olympia"
+# }, {
+#     "name": "West Virginia",
+#     "capital": "Charleston"
+# }, {
+#     "name": "Wisconsin",
+#     "capital": "Madison"
+# }, {
+#     "name": "Wyoming",
+#     "capital": "Cheyenne"
+# }]
 
 print("Hello! Welcome to the state capitals game! It's time to test your knowledge of the US and its 50 capitals!")
 
@@ -170,6 +171,8 @@ for state in (states):
         print("Wrong:", wrong, ":(")
 print("Game over! You got", correct, "correct and", wrong, "wrong.")
 
-restart = input("Would you like to play again? [Y/N]")
+restart = input("Would you like to play again? [Y/N]: ")
 if restart == "Y" or "y":
-    os.execv(sys.executable, ['python3'] + capitals_game.py)
+    os.system("python3 capitals_game.py")
+else:
+    print("Thanks for playing!")
